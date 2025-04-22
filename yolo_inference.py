9d9ea701e6a9357f07a52d46da5f4f54e49b58e6
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-04-22 20:04:34 +0530

    ------------[22/04/25]---------------

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,11 +1,13 @@
 # import ultralytics
 from ultralytics import YOLO
+# from ultralytics import SAM
 
 model = YOLO('yolov8x')
 
 # Inference
-# can give image/video
+# can give image/video:=>
 result  = model.predict(source='input_videos/input_video.mp4', 
-                        save=True)#, stream=True)
+                        save=True,)
+                        # stream=True)
 
 print(result)
